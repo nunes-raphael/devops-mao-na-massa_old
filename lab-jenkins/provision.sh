46386e9e4826045dff6876a874d3efbbc921bcdd
Raphael Nunes <oliveira.raphael@outlook.com>
2022-04-13 20:41:08 -0300

    Adicionado jenkinsfile

---
+++ lab-jenkins/provision.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+sudo yum update -y
+sudo yum install epel-release -y
+sudo yum install wget -y
+sudo wget --no-check-certificate -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo yum upgrade
+sudo yum install java-11-openjdk-devel -y
+sudo yum install jenkins -y
+sudo systemctl daemon-reload
+sudo systemctl start jenkins
+sudo yum install git -y
+
+### Instalacao do docker e docker compose
+sudo yum install yum-utils -y
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install docker-ce docker-ce-cli containerd.io -y
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo systemctl daemon-reload
+sudo systemctl restart docker
+sudo usermod -aG docker jenkins
